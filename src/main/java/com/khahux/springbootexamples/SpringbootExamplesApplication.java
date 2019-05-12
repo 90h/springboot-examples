8ba65623336f90141348f6060403be970328682a
maming <maming@0k.com>
2019-05-12 15:01:19 +0800

    first commit

---
+++ src/main/java/com/khahux/springbootexamples/SpringbootExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.khahux.springbootexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootExamplesApplication.class, args);
+    }
+
+}
