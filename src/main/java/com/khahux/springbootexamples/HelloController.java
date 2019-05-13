8ff000ede6651a6362ec60c19b3ae8fd42f09b20
tygertyger <imvintagetyger@github.com>
2019-05-13 10:24:48 +0800

    test

--- src/main/java/com/khahux/springbootexamples/HelloController.java
+++ src/main/java/com/khahux/springbootexamples/HelloController.java
@@ -3,13 +3,20 @@ package com.khahux.springbootexamples;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/")
 public class HelloController {
-    @GetMapping({"/", "/hello"})
-    public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
-        model.addAttribute("name", name);
-        return "hello";
+//    @GetMapping({"/", "/hello"})
+//    public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="World") String name) {
+//        model.addAttribute("name", name);
+//        return "hello";
+//    }
+
+    @RequestMapping("/")
+    public String login(){
+        return "home/login";
     }
 }
