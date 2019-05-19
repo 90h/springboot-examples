6b6e7d93ebfa20d9298aff6c9552987ee7cabd1f
tygertyger <imvintagetyger@github.com>
2019-05-19 14:41:05 +0800

    selenium

---
+++ src/main/java/com/khahux/seleniumdemo/Application.java
@@ -0,0 +1,19 @@
+package com.khahux.seleniumdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName Application
+ * @Description TODO
+ * @Author Lenovo
+ * @Date 2019/5/19 14:34
+ * @Version 1.0
+ **/
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
